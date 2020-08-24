87c023f77e48e0053795feb78f4a83141b6d4549
Anfernee Jervis <anferneejervis@gmail.com>
2020-08-24 17:45:17 +0100

    init commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+
+sudo apt update
+
+apt install -y emacs build-essential python3 python3-pip texlive-latex-extra texlive-fonts-recommended latexmk g++ haskell-platform sbcl default-jre default-jdk w3m lynx googler tmux git hub xclip 
